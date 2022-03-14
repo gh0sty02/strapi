2cec595e0b33980523dcb12ad6d205b7e555b8e1
Gustav Hansen <gustav.hansen@strapi.io>
2022-03-14 14:22:41 +0100

    FolderCardBody: Fix id structure

--- packages/core/helper-plugin/lib/src/components/FolderCard/FolderCardBody.js
+++ packages/core/helper-plugin/lib/src/components/FolderCard/FolderCardBody.js
@@ -3,14 +3,19 @@ import styled from 'styled-components';
 
 import { Flex } from '@strapi/design-system/Flex';
 
+import { useFolderCard } from './FolderCardContext';
+
 const StyledBox = styled(Flex)`
   user-select: none;
 `;
 
 export const FolderCardBody = props => {
+  const { id } = useFolderCard();
+
   return (
     <StyledBox
       {...props}
+      id={`${id}-title`}
       alignItems="flex-start"
       direction="column"
       position="relative"
